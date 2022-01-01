719f1a99cb11041e04b63d4a9920bd2fd4ec8c75
Andrew Samaha <andrew.samaha@gmail.com>
2021-12-31 19:03:08 -0500

    tidying and changed viewport movement to camera

--- app/static/main.js
+++ app/static/main.js
@@ -1,9 +1,12 @@
 import './css/style.css'
 import * as THREE from 'three';
+import ViewportCamera from './lib/viewportCamera';
+import ViewportTiles from './lib/viewportTiles';
+import Thing from './lib/entities/Thing';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new ViewportCamera()
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
@@ -12,86 +15,12 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight );
 
-camera.position.set(0, 25, 20);
-camera.rotation.set(-Math.PI*.35, 0, 0);
-
-const objects = [];
-
 //renderer.render( scene, camera );
-class Thing {
-  constructor( geometry, material, scene ) {
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add(mesh);
-    objects.push(mesh);
-    return mesh;
-  }
-};
 
-class ViewportTiles {
 
-  constructor( scene, width, depth ) {
-    const grassTextures = [
-      new THREE.TextureLoader().load('img/textures/grass/grass1.jpg'),
-      new THREE.TextureLoader().load('img/textures/grass/grass2.jpg'),
-      new THREE.TextureLoader().load('img/textures/grass/grass3.jpg'),
-      new THREE.TextureLoader().load('img/textures/grass/grass4.jpg'),
-      new THREE.TextureLoader().load('img/textures/grass/grass5.jpg')
-    ];
 
-    const tileSize = 5;
-    const tiles = new Array(width * depth);
-    console.log(tiles.length);
-    const velocity = new THREE.Vector2();
-    const moveFriction = 0.075;
-    const maxVelocity = .5;
 
-    var tileIndex = 0;
-    for (var w = 0; w < width; w++) {
-      for (var d = 0; d < depth; d++) {
-        tileIndex++;
-        tiles[tileIndex] = new Thing(
-          new THREE.PlaneGeometry(tileSize,tileSize),
-          //new THREE.BoxGeometry(tileSize, tileSize, .1),
-          new THREE.MeshStandardMaterial( { map: grassTextures[THREE.MathUtils.randInt(0,grassTextures.length-1)] } ),
-          scene      
-        );
-        tiles[tileIndex].position.set(
-          w*tileSize - (width*tileSize/2), 
-          0,
-          d*tileSize - (depth*tileSize/2)
-          );
-        tiles[tileIndex].rotation.set(-Math.PI/2,0,0)
-      }
-    }
 
-    const vT = {
-      tiles,
-      translate: (x, z) => {
-        tiles.forEach((tile) => {
-          tile.position.x += x;
-          tile.position.z += z;
-        })
-      },
-      applyFriction: (velocity) => {
-        if (Math.abs(velocity) > moveFriction) return velocity * (1-moveFriction)
-        else return 0
-      },
-      push: (x, z) => {
-        velocity.x = Math.min(Math.max(velocity.x + x, -maxVelocity), maxVelocity);
-        velocity.y = Math.min(Math.max(velocity.y + z, -maxVelocity), maxVelocity);
-        console.log('push velocity',velocity.x,velocity.y)
-      },
-      physicsTic: () => {
-        velocity.x = vT.applyFriction(velocity.x);
-        velocity.y = vT.applyFriction(velocity.y);
-        if (velocity.x || velocity.y) vT.translate(velocity.x, velocity.y);
-      }
-
-    }
-
-    return vT;
-  }
-}
 
 const viewportTiles = new ViewportTiles( scene, 25, 25 );
 console.log(viewportTiles.tiles.length);
@@ -151,7 +80,8 @@ function onMouseMove(event) {
 function animate() {
   requestAnimationFrame( animate );
   cube.rotation.z += 0.01;
-  viewportTiles.physicsTic();
+  //viewportTiles.physicsTic();
+  camera.physicsTic();
   renderer.render( scene, camera );
 }
 
@@ -161,20 +91,9 @@ animate();
 var xSpeed = 0.5;
 var ySpeed = 0.5;
 
-document.addEventListener("keydown", onDocumentKeyDown, false);
-function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-    if (keyCode == 38) {
-      console.log('up');
-      viewportTiles.push(0,ySpeed);
-    } else if (keyCode == 37) {
-      console.log('left');
-        viewportTiles.push(xSpeed,0);
-    } else if (keyCode == 39) {
-      console.log('right');
-      viewportTiles.push(-xSpeed,0);
-    } else if (keyCode == 40) {
-      console.log('down');
-      viewportTiles.push(0,-ySpeed);
-    }
-};
+document.addEventListener("keydown", (event) => {
+  camera.keyboardScroll(event.code, 'down');
+}, false);
+document.addEventListener("keyup", (event) => {
+  camera.keyboardScroll(event.code, 'up');
+}, false);
